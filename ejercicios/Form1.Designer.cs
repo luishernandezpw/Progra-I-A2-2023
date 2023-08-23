e48d8deacb5a9e401d850d7a5d007c9e5a0752a0
Luis Hernandez <sarco_luis@hotmail.com>
2023-08-23 09:55:37 -0600

    Se creo la funcion de suma y se dejo unos ejercicios

--- ejercicios/Form1.Designer.cs
+++ ejercicios/Form1.Designer.cs
@@ -31,12 +31,17 @@
             this.btnProcesar = new System.Windows.Forms.Button();
             this.lblNTablaMultiplicar = new System.Windows.Forms.Label();
             this.txtNTablaMultiplicar = new System.Windows.Forms.TextBox();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.btnSumar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnProcesar
             // 
             this.btnProcesar.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnProcesar.Location = new System.Drawing.Point(84, 98);
+            this.btnProcesar.Location = new System.Drawing.Point(84, 133);
             this.btnProcesar.Name = "btnProcesar";
             this.btnProcesar.Size = new System.Drawing.Size(138, 68);
             this.btnProcesar.TabIndex = 0;
@@ -60,11 +65,59 @@
             this.txtNTablaMultiplicar.Size = new System.Drawing.Size(100, 20);
             this.txtNTablaMultiplicar.TabIndex = 2;
             // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Location = new System.Drawing.Point(241, 44);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(41, 13);
+            this.lblNum1.TabIndex = 3;
+            this.lblNum1.Text = "Num 1:";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(288, 41);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(44, 20);
+            this.txtNum1.TabIndex = 4;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(385, 41);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(44, 20);
+            this.txtNum2.TabIndex = 6;
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Location = new System.Drawing.Point(338, 44);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(41, 13);
+            this.lblNum2.TabIndex = 5;
+            this.lblNum2.Text = "Num 2:";
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSumar.Location = new System.Drawing.Point(264, 133);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(138, 68);
+            this.btnSumar.TabIndex = 7;
+            this.btnSumar.Text = "Sumar";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(312, 250);
+            this.ClientSize = new System.Drawing.Size(479, 250);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.lblNum1);
             this.Controls.Add(this.txtNTablaMultiplicar);
             this.Controls.Add(this.lblNTablaMultiplicar);
             this.Controls.Add(this.btnProcesar);
@@ -80,6 +133,11 @@
         private System.Windows.Forms.Button btnProcesar;
         private System.Windows.Forms.Label lblNTablaMultiplicar;
         private System.Windows.Forms.TextBox txtNTablaMultiplicar;
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.Button btnSumar;
     }
 }
 
--- ejercicios/Form1.cs
+++ ejercicios/Form1.cs
@@ -26,5 +26,35 @@ namespace ejercicios
             }
             MessageBox.Show(msg);
         }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            double num1 = double.Parse(txtNum1.Text), 
+                num2 = double.Parse(txtNum2.Text), 
+                respuesta = sumar(num1, num2);
+            MessageBox.Show("Respuesta: "+ respuesta);
+
+            //Crear un programa en C# que genere la secuencia de los numeros fibonacci.
+            /*
+            0 + 1 = 1
+            1 + 0 = 1
+            1 + 1 = 2
+            2 + 1 = 3
+            3 + 2 = 5
+            5 + 3 = 8
+            8 + 5 = 13
+            13 + 8 = 21
+            21 + 13 = 34
+            34 + 21 = 55
+            ...
+             */
+            //Cree un programa en C# que determine si un numero es par o impar.
+            //Cree un programa en C# que determine si un numero es primo o no.
+        }
+
+        double sumar(double num1, double num2)
+        {
+            return num1 + num2;
+        }
     }
 }
